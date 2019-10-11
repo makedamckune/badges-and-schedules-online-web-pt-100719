1785b4df9b8290ec1f62051994f6833d77b5097d
Makeda P Mckune <makedamckune@gmail.com>
2019-10-11 07:28:25 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def batch_badge_creator
+badge_maker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  badge_maker.each do |batch_badge_creator|
+  puts "Hello, my name is #{badge_maker}."
+end 
+
+def assign_rooms
+  badge_maker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  counter = 1 
+  badge_maker.each do |badge_maker| 
+    Puts "Hello #{badge_maker}, you'll be assigned to room #{counter}!"
+    counter += 1 
+  end
