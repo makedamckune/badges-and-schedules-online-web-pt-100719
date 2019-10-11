62d48c9d1a868c5fc8c8b62f2110c89b434f4937
Makeda P Mckune <makedamckune@gmail.com>
2019-10-11 07:14:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,4 @@
-# Write your code here.
+batch_badge_creator = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+batch_badge_creator.each do |batch_badge_creator|
+  puts "Hello, my name is #{batch_badge_creator}."
+end 
