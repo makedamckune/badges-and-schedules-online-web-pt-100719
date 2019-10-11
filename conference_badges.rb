c736677ab1eda16a4483d8317d8150c541ea713c
Makeda P Mckune <makedamckune@gmail.com>
2019-10-11 07:17:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,4 @@
+def batch_badge_creator
 badge_maker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   badge_maker.each do |batch_badge_creator|
   puts "Hello, my name is #{badge_maker}."
