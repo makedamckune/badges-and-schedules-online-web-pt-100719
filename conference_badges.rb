785db64c7dbfc591af929319ed3feb97bf3aa8a6
Makeda P Mckune <makedamckune@gmail.com>
2019-10-11 07:26:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,10 @@ badge_maker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   badge_maker.each do |batch_badge_creator|
   puts "Hello, my name is #{badge_maker}."
 end 
+
+def assign_rooms
+  badge_maker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  counter = 1 
+  badge_maker.each do |badge_maker| 
+    Puts "Hello #{badge_maker}, you'll be assigned to room #{counter}!"
+  end
